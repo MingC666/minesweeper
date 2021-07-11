cff05d652eae6434007167475aaea1dcfb928a2d
MingC666 <57696323+MingC666@users.noreply.github.com>
2021-07-11 04:20:28 -0700

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -32,7 +32,7 @@ class MyAI( AI ):
 		for i in range(self.row):
 				for j in range(self.col):
 					self.uncovered.append((i,j))
-
+		self.uncovered.remove((self.x,self.y))
 		self.frotier = []
 		########################################################################
 		#							YOUR CODE ENDS							   #
@@ -56,8 +56,24 @@ class MyAI( AI ):
 					if(i<0 or i>self.row or j<0 or j>self.col):
 						continue
 					Action(AI.Action.UNCOVER, i, j)
+					self.uncovered.remove((i,j))
 					self.frotier.append((i,j))
 
+		else: #number is 1, then uncover all except its neighboor.
+			for i in range(self.row):
+				for j in range(self.col):
+					#if(i==self.x+1 or i==self.x+1 or)
+					if(i-self.x>=-1 and i-self.x<=1  and j-self.y>=-1 and j-self.y<=1):
+						continue
+					Action(AI.Action.UNCOVER, i, j)
+					self.uncovered.remove((i,j))
+					
+		if(self.uncovered != [] ):
+			temp = self.uncovered.pop(0)
+			return Action(AI.Action.UNCOVER, temp[0], temp[1])
+		
+		#return Action(AI.Action.LEAVE)
+		
 		
 
 
