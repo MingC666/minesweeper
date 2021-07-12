ff62a73e209bba1883f41d6a566bf224a76f6d82
MingC666 <57696323+MingC666@users.noreply.github.com>
2021-07-11 20:55:06 -0700

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -47,16 +47,7 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
-		while(self.move>0):
-			temp=self.to_uncovered.pop(0)
-			print(temp[0], temp[1])
-			self.move-=1
-			return Action(AI.Action.UNCOVER, temp[0], temp[1])
-		
-
-		# Uncover all if number = 0
-		print(self.x, self.y)
-		if(0 == number and self.move==0):
+		if(0 == number):
 			for i in range(self.x-1, self.x+2):
 				for j in range(self.y-1, self.y+2):
 					# boundary checking
@@ -64,24 +55,9 @@ class MyAI( AI ):
 						continue
 					if(i==self.x and j==self.y): #i skip itself
 						continue
-					self.to_uncovered.append((i,j))
-			print(len(self.to_uncovered))
-			temp=self.to_uncovered.pop(0)
-			self.move=len(self.to_uncovered)
 			return Action(AI.Action.UNCOVER, temp[0], temp[1])
 
-		else: #number is 1, then uncover all except its neighboor.
-			for i in range(self.row):
-				for j in range(self.col):
-					#if(i==self.x+1 or i==self.x+1 or)
-					if(i-self.x>=-1 and i-self.x<=1  and j-self.y>=-1 and j-self.y<=1):
-						continue
-					self.to_uncovered.append((i,j))
-					self.uncovered.remove((i,j))
-					
-		while(self.to_uncovered != [] ):
-			temp = self.uncovered.pop(0)
-			return Action(AI.Action.UNCOVER, temp[0], temp[1])
+		
 		
 		#return Action(AI.Action.LEAVE)
 		
