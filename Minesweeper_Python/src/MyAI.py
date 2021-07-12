02a3cb25b39910d3fdc61ea347a1650c782f03ca
MingC666 <57696323+MingC666@users.noreply.github.com>
2021-07-11 20:48:47 -0700

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -29,9 +29,7 @@ class MyAI( AI ):
 		self.x = startX 
 		self.y = startY
 		self.uncovered =[]
-		self.to_uncovered = []
-		self.covered = []
-		self.danger = []
+		self.to_uncovered =[]
 		self.move=0
 		for i in range(self.row):
 				for j in range(self.col):
@@ -49,10 +47,16 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
+		while(self.move>0):
+			temp=self.to_uncovered.pop(0)
+			print(temp[0], temp[1])
+			self.move-=1
+			return Action(AI.Action.UNCOVER, temp[0], temp[1])
 		
 
 		# Uncover all if number = 0
-		if(0 == number):
+		print(self.x, self.y)
+		if(0 == number and self.move==0):
 			for i in range(self.x-1, self.x+2):
 				for j in range(self.y-1, self.y+2):
 					# boundary checking
@@ -60,24 +64,25 @@ class MyAI( AI ):
 						continue
 					if(i==self.x and j==self.y): #i skip itself
 						continue
-					if((i,j) not in self.uncovered):
-						self.to_uncovered.append((i,j))
-						self.uncovered.append((i,j))
-						self.frotier.append((i,j))
-
-		elif(1==number): #number is 1, find the one that only have 1 covered, which is mine--- only for 5*5
-			for i in range(self.x-1, self.x+2):
-				for j in range(self.y-1, self.y+2):
-					if((i,j) not in self.uncovered):
-						self.danger.append((i,j))
-
-
-		while(self.to_uncovered != []):
-			temp = self.to_uncovered.pop(0)
+					self.to_uncovered.append((i,j))
+			print(len(self.to_uncovered))
+			temp=self.to_uncovered.pop(0)
+			self.move=len(self.to_uncovered)
 			return Action(AI.Action.UNCOVER, temp[0], temp[1])
 
-		print(len(self.danger))			
-		# uncovered all reset tile
+		else: #number is 1, then uncover all except its neighboor.
+			for i in range(self.row):
+				for j in range(self.col):
+					#if(i==self.x+1 or i==self.x+1 or)
+					if(i-self.x>=-1 and i-self.x<=1  and j-self.y>=-1 and j-self.y<=1):
+						continue
+					self.to_uncovered.append((i,j))
+					self.uncovered.remove((i,j))
+					
+		while(self.to_uncovered != [] ):
+			temp = self.uncovered.pop(0)
+			return Action(AI.Action.UNCOVER, temp[0], temp[1])
+		
 		#return Action(AI.Action.LEAVE)
 		
 		
