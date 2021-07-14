4c165158165b2f30bd89f67f78d806174f6ad6a3
MingC666 <57696323+MingC666@users.noreply.github.com>
2021-07-14 00:38:47 -0700

    Update MyAI.py

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -28,90 +28,62 @@ class MyAI( AI ):
 		self.totalMines = totalMines
 		self.x = startX 
 		self.y = startY
-		self.lastx = startX
-		self.lasty = startY
 		
 
 		self.covered = []
 		self.to_uncovered = []
-		self.safelist = []
-		#initial covered list
-		#for i in range(self.row-1):
-			#for j in range(self.col-1):
-					#self.covered.append((i,j))
-			
-		#initial map list
-		rows, cols = (self.row, self.col)
-		self.map = [[-1 for i in range(cols)] for j in range(rows)]
+		self.checkedlist = []
 
+		# add fisrt checked point
+		self.checkedlist.append((self.x, self.y))
+		
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
 
 
-		
 	def getAction(self, number: int) -> "Action Object":
 
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
 		
+		# Uncover all surrounding that not uncovered yet, if number = 0
 
-		# Uncover all if number = 0
-		if(0 == number):
-			self.safelist.append((self.lastx, self.lasty))
-			for i in range(self.lastx-1, self.lastx+2):
-				for j in range(self.lasty-1, self.lasty+2):
+		if(0 == number and len(self.checkedlist)<self.row*self.col-self.totalMines):  # number of unchecked < total tile - #mine
+				# i=2, j=0
+				# rang(1,4)
+			for i in range(self.x-1, self.x+2):
+				for j in range(self.y-1, self.y+2):
 					# boundary checking
 					if(i<0 or i>self.row-1 or j<0 or j>self.col-1):
 						continue
-					self.map[i][j] = 0
-					if((i,j)!=(self.x,self.y) and ((i,j) not in self.safelist) and ((i,j) not in self.to_uncovered)):
+					if((i,j) not in self.checkedlist):
+						#print("I am in 000 area!! ", (self.x,self.y), "will uncovered", (i,j) )
+						self.checkedlist.append((i,j))
 						self.to_uncovered.append((i,j))
-			
-
-		else: #number is 1
-			self.map[self.lastx][self.lasty] = number
-
+		
+		
+		# checking if unmber is not 0
+		if(number!=0 and len(self.checkedlist)<self.row*self.col-self.totalMines):  # number of unchecked < total tile - #mine
+			for i in range(self.row):
+				for j in range(self.col):
+					if( (i>self.x-1 and i<self.x+2) or (j>self.y-1 and j<self.y+2) ):
+						continue
+					if( (i,j) not in self.checkedlist or (i,j) in self.covered):
+						#print("I am in MINE area!! ", (self.x,self.y), "will uncovered", (i,j) )
+						self.checkedlist.append((i,j))
+						self.to_uncovered.append((i,j))
 
 		if(self.to_uncovered != []):
+			#print(len(self.checkedlist))
 			temp = self.to_uncovered.pop(0)
-			self.lastx=temp[0]
-			self.lasty=temp[1]
+			self.x = temp[0]
+			self.y = temp[1]
 			return Action(AI.Action.UNCOVER, temp[0], temp[1])
 
 
-	
-		#if(self.to_uncovered != []):
-			#temp = self.to_uncovered.pop(0)
-			#return Action(AI.Action.UNCOVER, temp[0], temp[1])
-		for i in range(self.row):
-			for j in range(self.col):
-				if self.map[i][j] == -1:
-					self.covered.append((i,j))
-
-
-		count = 0
-		minelist = []
-		for i in range(1, self.row-1):
-			for j in range(1, self.row-1):
-				if (self.map[i][j]) == -1:
-					minelist.append((i,j))
-					count += 1
-		
-		if(count == 1):
-			self.covered.remove(minelist[0])
-
-		for tile in self.covered:
-			return Action(AI.Action.UNCOVER, tile[0], tile[1])
-
-			# check if the map temparr  are all 0, if yes, (j,k) are mine, uncovered all self.covered except (j,k)
-
-
-				
-
-   					 
-							
+		return Action(AI.Action.LEAVE)	
 							 
 	
 		########################################################################
